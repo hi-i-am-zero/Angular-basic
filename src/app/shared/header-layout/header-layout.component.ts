8822109888c52931a58e8c5afe92ff79fe074075
h-i-am-zero <nguyenhoangviet531@gmail.com>
2025-04-17 10:51:13 +0700

    lesson2: how to import component file

---
+++ src/app/shared/header-layout/header-layout.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+@Component({
+    selector: 'header-layout',
+    standalone: true, //Đánh dấu component này là độc lập, không cần khai báo trong NgModule.
+    templateUrl: './header-layout.component.html',
+    styleUrl: './header-layout.component.scss'
+})
+
+export class HeaderLayoutComponent{
+
+}
