b7473aa6ffa4f88c3229902b7d910ae9894cde1c
Zero <nguyenhoangviet531@gmail.com>
2025-04-20 22:30:17 +0700

    lesson19: form control

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,3 +1,4 @@
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { BlogService } from '../../services/BlogService';
 import { ProductItems } from '../shared/types/productItem';
 import { Component, OnInit } from '@angular/core';
@@ -5,10 +6,18 @@ import { ActivatedRoute, RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
+  name = new FormControl('');
+  price = new FormControl('');
+
   constructor(private blogService: BlogService) {}
+
+  handleAddCart() {
+    console.log(this.name.value);
+    console.log(this.price.value);
+  }
 }
