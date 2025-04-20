89faa358a62b62b3b8177e30b00104a661ee28b4
Zero <nguyenhoangviet531@gmail.com>
2025-04-21 00:06:13 +0700

    lesson21: validate form and submit form

--- src/app/create/create.component.ts
+++ src/app/create/create.component.ts
@@ -1,19 +1,20 @@
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { BlogService } from '../../services/BlogService';
-import { ProductItems } from '../shared/types/productItem';
+import { BlogItem, ProductItems } from '../shared/types/productItem';
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, RouterOutlet } from '@angular/router';
+import { ActivatedRoute, Router, RouterOutlet } from '@angular/router';
+import { NgIf } from '@angular/common';
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgIf],
   templateUrl: './create.component.html',
   styleUrl: './create.component.scss'
 })
 export class CreateComponent {
   product = new FormGroup({
-    name: new FormControl(''),
-    price: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    price: new FormControl('', Validators.required),
     
   })
 
@@ -25,10 +26,21 @@ export class CreateComponent {
     return this.product.get('price');
   }
 
-  constructor(private blogService: BlogService) {}
+  constructor(private blogService: BlogService, private router: Router) {}
 
   handleAddCart() {
-    console.log(this.name?.value);
-    console.log(this.price?.value);
+    if(this.name?.hasError('required') || this.price?.hasError('required'))
+      return;
+    const blogItem : BlogItem = {
+      id: Math.random(),
+      title: String(this.name?.value),
+      body: String(this.price?.value),
+      author: 'mario',
+    }
+    this.blogService.postBlog(blogItem).subscribe(({ data }: any) => {
+      if(data.id) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 }
--- src/services/BlogService.ts
+++ src/services/BlogService.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ResponseData } from '../app/shared/types/responseData';
-import { ProductItems } from '../app/shared/types/productItem';
+import { BlogItem, ProductItems } from '../app/shared/types/productItem';
 import { Observable } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
@@ -12,10 +12,14 @@ export class BlogService {
     constructor(private http: HttpClient) {}
 
     getBlogs(): Observable<ResponseData<ProductItems[]>> {
-        return this.http.get<any>('https://ninedev-api.vercel.app/blogs');
+        return this.http.get<any>(`https://ninedev-api.vercel.app/blogs`);
     }
 
     detailBlog(id: number): Observable<ResponseData<ProductItems>> {
         return this.http.get<any>(`https://ninedev-api.vercel.app/blogs/${id}`);
+    }
+
+    postBlog(blogItem: BlogItem):Observable<ResponseData<ProductItems>> {
+        return this.http.post<any>(`https://ninedev-api.vercel.app/blogs`, blogItem);
     }
 }
